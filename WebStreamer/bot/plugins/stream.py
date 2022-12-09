cc46c1e18cc5138894126008328a544b49f944b6
Raman456123 <103272817+Raman456123@users.noreply.github.com>
2022-12-09 15:58:36 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -26,9 +26,9 @@ async def media_receive_handler(_, m: Message):
         file_name = file.file_name
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
-    stream_links = f"https://KalviDudes.in"
+    stream_links = f"https://t.me/cinemadudes1"
     await m.reply_text(
         text=f"https://url.mysteryfacts.xyz/st?api=47b8f21cad30ded04afc99547130e71944e3c0d7&url={stream_link}",
         quote=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Learn something interesting 🤔', url=stream_links)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Join our Channel', url=stream_links)]])
     )
